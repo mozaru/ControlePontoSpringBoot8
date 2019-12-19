b3ddd8cb387eaac02a40f3acd92f18304979ba87
Mozar Silva <mozar.silva@11tech.com.br>
2019-12-19 19:51:50 -0300

    api rest ok

---
+++ src/main/java/br/com/tech/ControlePonto/controllers/CadastroPJController.java
@@ -0,0 +1,63 @@
+package br.com.tech.ControlePonto.controllers;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.*;
+import java.util.*;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class CadastroPJController {
+
+    @GetMapping(path = "/test")
+    public ResponseEntity<String> getAll()
+    {
+        return ResponseEntity.ok("teste");
+    }
+
+    @GetMapping(path = "/map")
+    public ResponseEntity<Map<String, Object>> getmap()
+    {
+        Map<String, Object> m = new HashMap<String, Object>();
+        m.put("id",10);
+        m.put("Nome","Joao Map");
+        return ResponseEntity.ok(m);
+    }
+
+    @GetMapping(path = "/json")
+    public ResponseEntity<Object> getjson()
+    {
+        return ResponseEntity.ok(new Object() { public final int id=10;
+                                                public final String Nome="Joao Json";});
+    }
+
+    @GetMapping(path = "/oi/{nome}")
+    public ResponseEntity<Object> getoi(@PathVariable("nome") String nome)
+    {
+        return ResponseEntity.ok(new Object() { public final int id=10;
+                                                public final String Nome=nome;});
+    }
+
+    @GetMapping(path = "/msg")
+    public ResponseEntity<Object> getmsg(
+         @RequestParam(value = "id", defaultValue="10") int aid,
+         @RequestParam(value = "nome", defaultValue="mozar") String anome)
+    {
+        return ResponseEntity.ok(new Object() { public final int id=aid;
+                                                public final String Nome=anome;});
+    }
+
+    @PostMapping(path = "/add", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<String> getmsg(@RequestBody Map<String, Object> dados)
+    {   
+        try{
+            System.out.println("nome:"+dados.get("nome"));
+            System.out.println("id:"+dados.get("id"));
+            return ResponseEntity.ok("inserido com sucesso!");
+        }catch(Exception e)
+        {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
