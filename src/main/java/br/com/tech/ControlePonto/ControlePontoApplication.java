5a17d78ca38f88ceb7bf7b48f7a391ba5bf906e3
Mozar Silva <mozar.silva@11tech.com.br>
2019-12-19 16:29:52 -0300

    criação do projeto

---
+++ src/main/java/br/com/tech/ControlePonto/ControlePontoApplication.java
@@ -0,0 +1,14 @@
+package br.com.tech.ControlePonto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlePontoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("oi, controle de ponto");
+		SpringApplication.run(ControlePontoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tech/ControlePonto/ControlePontoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tech.ControlePonto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlePontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
